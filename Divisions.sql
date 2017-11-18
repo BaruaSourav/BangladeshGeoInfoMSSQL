25b276f9d688adf7a05bcacf78494974aead7422
Sourav Barua <sourav.shr6@hotmail.com>
2017-11-19 01:13:22 +0600

    District and Division Initial DDL

---
+++ Divisions.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE divisions (
+  [id] int check ([id] > 0) NOT NULL IDENTITY,
+  [name] varchar(30) NOT NULL,
+  [bn_name] varchar(50) NOT NULL,
+  PRIMARY KEY ([id])
+)    ;
+
+SET IDENTITY_INSERT Divisions ON;
+INSERT INTO Divisions ([id], [name], [bn_name]) VALUES
+(1, 'Barisal', N'বরিশাল'),
+(2, 'Chittagong', N'চট্টগ্রাম'),
+(3, 'Dhaka', N'ঢাকা'),
+(4, 'Khulna', N'খুলনা'),
+(5, 'Rajshahi', N'রাজশাহী'),
+(6, 'Rangpur', N'রংপুর'),
+(7, 'Sylhet', N'সিলেট');
+SET IDENTITY_INSERT Divisions OFF;
